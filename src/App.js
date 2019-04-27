973d9f307e60fce111e6c6e4d39e4e641ec62676
Vaselek <burzhuas@gmail.com>
2019-04-27 14:24:51 +0600

    Add packages and configured store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import {Container} from "reactstrap";
+import {Route, Switch, withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import {NotificationContainer} from "react-notifications";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {logoutUser} from "./store/actions/usersActions";
+
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import Products from "./containers/Products/Products";
+import NewProduct from "./containers/NewProduct/NewProduct";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import Sidebar from "./components/Sidebar/Sidebar";
+import './App.css'
+import {fetchCategories} from "./store/actions/categoriesActions";
+import {fetchProducts} from "./store/actions/productsActions";
+import Product from "./containers/Product/Product";
+
+class App extends Component {
+    componentDidMount() {
+      this.props.fetchCategories()
+    }
+    render() {
+        return (
+            <Fragment>
+                <NotificationContainer/>
+                <header>
+                    <Toolbar
+                        user={this.props.user}
+                        logout={this.props.logoutUser}
+                    />
+                </header>
+                <Container className='Main-container'>
+                  <Sidebar categories={this.props.categories}
+                           fetchProducts={this.props.fetchProducts}/>
+                  <div className='Main-column'>
+                    <Switch>
+                        <Route path="/" exact component={Products}/>
+                        <Route path="/products/new" exact component={NewProduct}/>
+                        <Route path="/product/:id" exact component={Product}/>
+                        <Route path="/register" exact component={Register}/>
+                        <Route path="/login" exact component={Login}/>
+                    </Switch>
+                  </div>
+                </Container>
+            </Fragment>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    user: state.users.user,
+    categories: state.categories.categories
+});
+
+const mapDispatchToProps = dispatch => ({
+    logoutUser: () => dispatch(logoutUser()),
+    fetchCategories: () => dispatch(fetchCategories()),
+    fetchProducts: (categoryId) => dispatch(fetchProducts(categoryId))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
